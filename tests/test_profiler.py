9f4f563fd65f78d5d5d35eae9998425f93e9119f
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2019-11-11 16:13:13 +0100

    Add test for closing the profiler connection

--- tests/test_profiler.py
+++ tests/test_profiler.py
@@ -3,6 +3,11 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 # Copyright 1997 - July 2008 CWI, August 2008 - 2016 MonetDB B.V.
+"""
+Tests for the profiler connection
+"""
+
+
 import unittest
 from mock import patch
 import pymonetdb
@@ -19,6 +24,13 @@ class ProfilerTest(unittest.TestCase):
 
     @patch('pymonetdb.mapi.Connection._getblock')
     def test_profiler_connection(self, mock_getblock):
+        """Test opening a connection to the profiler"""
         response = '{"key":"value"}\n'  # A random JSON object
         mock_getblock.return_value = response
         self.assertEqual(self.conn.read_object(), response[:-1])
+
+    @patch('pymonetdb.mapi.Connection.disconnect')
+    def test_profiler_disconnect(self, mock_disconnect):
+        """Test closing a profiler connection"""
+        self.conn.disconnect()
+        mock_disconnect.assert_called_once()
